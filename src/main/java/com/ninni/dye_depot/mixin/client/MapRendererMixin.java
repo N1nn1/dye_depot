096e84a6e70b18e11d96ff79f03ef642866c4d3a
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2024-01-30 05:46:51 +0100

    fine tuning

--- src/main/java/com/ninni/dye_depot/mixin/client/MapRendererMixin.java
+++ src/main/java/com/ninni/dye_depot/mixin/client/MapRendererMixin.java
@@ -49,11 +49,12 @@ public abstract class MapRendererMixin {
                     float m = (float)(b % 16 + 1) / 16.0F;
                     float n = (float)(b / 16 + 1) / 16.0F;
                     Matrix4f matrix4f2 = poseStack.last().pose();
+                    float j = (l * -0.001F) - 0.01F;
                     VertexConsumer vertexConsumer2 = multiBufferSource.getBuffer(RenderType.text(new ResourceLocation(DyeDepot.MOD_ID,"textures/map/map_icons.png")));
-                    vertexConsumer2.vertex(matrix4f2, -1.0F, 1.0F, (float)l * -0.0019F).color(255, 255, 255, 255).uv(g, h).uv2(i).endVertex();
-                    vertexConsumer2.vertex(matrix4f2, 1.0F, 1.0F, (float)l * -0.0019F).color(255, 255, 255, 255).uv(m, h).uv2(i).endVertex();
-                    vertexConsumer2.vertex(matrix4f2, 1.0F, -1.0F, (float)l * -0.0019F).color(255, 255, 255, 255).uv(m, n).uv2(i).endVertex();
-                    vertexConsumer2.vertex(matrix4f2, -1.0F, -1.0F, (float)l * -0.0019F).color(255, 255, 255, 255).uv(g, n).uv2(i).endVertex();
+                    vertexConsumer2.vertex(matrix4f2, -1.0F, 1.0F, j).color(255, 255, 255, 255).uv(g, h).uv2(i).endVertex();
+                    vertexConsumer2.vertex(matrix4f2, 1.0F, 1.0F, j).color(255, 255, 255, 255).uv(m, h).uv2(i).endVertex();
+                    vertexConsumer2.vertex(matrix4f2, 1.0F, -1.0F, j).color(255, 255, 255, 255).uv(m, n).uv2(i).endVertex();
+                    vertexConsumer2.vertex(matrix4f2, -1.0F, -1.0F, j).color(255, 255, 255, 255).uv(g, n).uv2(i).endVertex();
                     poseStack.popPose();
                 }
             }
