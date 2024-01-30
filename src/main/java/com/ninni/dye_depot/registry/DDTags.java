b08705c9f69019f8e523152c0dd9b61bf2f3c98f
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2024-01-30 22:17:55 +0100

    All recipes

---
+++ src/main/java/com/ninni/dye_depot/registry/DDTags.java
@@ -0,0 +1,12 @@
+package com.ninni.dye_depot.registry;
+
+import com.ninni.dye_depot.DyeDepot;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+public interface DDTags {
+    //Item tags
+    TagKey<Item> SMELTS_INTO_CORAL_DYE = TagKey.create(Registries.ITEM, new ResourceLocation(DyeDepot.MOD_ID, "smelts_into_coral_dye"));
+}
