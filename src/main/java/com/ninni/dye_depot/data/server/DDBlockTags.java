8687ec5bd8ccee9b3bd01eb9498f64e5fb7950a9
Niklas Widmann <nik@somethingcatchy.net>
2025-09-26 14:36:24 +0200

    reference color tags in base & add non-cleanable tags for dye baskets

--- src/main/java/com/ninni/dye_depot/data/server/DDBlockTags.java
+++ src/main/java/com/ninni/dye_depot/data/server/DDBlockTags.java
@@ -4,7 +4,11 @@ import com.ninni.dye_depot.DyeDepot;
 import com.ninni.dye_depot.data.ModCompat;
 import com.ninni.dye_depot.registry.DDBlocks;
 import com.ninni.dye_depot.registry.DyedHolders;
+
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+import net.mehvahdjukaar.supplementaries.reg.ModTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
@@ -29,6 +33,8 @@ public class DDBlockTags extends BlockTagsProvider {
         var lookup = this.lookup.join();
         var blockLookup = lookup.lookupOrThrow(Registries.BLOCK);
 
+        groupDyedTag("dyed");
+
         tagDyed(DDBlocks.SHULKER_BOXES, BlockTags.SHULKER_BOXES);
         tagDyed(DDBlocks.BANNERS, BlockTags.BANNERS);
         tagDyed(DDBlocks.WALL_BANNERS, BlockTags.BANNERS);
@@ -43,7 +49,7 @@ public class DDBlockTags extends BlockTagsProvider {
         tagDyed(DDBlocks.CONCRETE_POWDER, BlockTags.MINEABLE_WITH_SHOVEL);
         tagDyed(DDBlocks.STAINED_GLASS, loaderTag("glass_blocks"), BlockTags.IMPERMEABLE);
         tagDyed(DDBlocks.STAINED_GLASS_PANES, loaderTag("glass_panes"), BlockTags.IMPERMEABLE);
-        tagDyed(DDBlocks.DYE_BASKETS, BlockTags.MINEABLE_WITH_HOE);
+        tagDyed(DDBlocks.DYE_BASKETS, BlockTags.MINEABLE_WITH_HOE, ModTags.SOAP_BLACKLIST_BLOCK);
 
         tagDyed(ModCompat.supplementariesHolders(blockLookup, "candle_holder"), supplementariesTag("candle_holders"));
         tagDyed(ModCompat.supplementariesSquaredHolders(blockLookup, "gold_candle_holder"), supplementariesTag("candle_holders"), BlockTags.GUARDED_BY_PIGLINS);
@@ -58,12 +64,18 @@ public class DDBlockTags extends BlockTagsProvider {
                 .forEach(it -> tag(tag).addOptional(it));
     }
 
+    private void groupDyedTag(String base) {
+        Stream.concat(DyedHolders.vanillaColors(), DyedHolders.modColors()).forEach(color ->
+                tag(loaderTag(base)).addOptionalTag(loaderTag(base + "/" + color))
+        );
+    }
+
     @SafeVarargs
     private void tagDyed(DyedHolders<?, Block> values, TagKey<Block>... additionalTags) {
         values.forEach((dye, block) -> {
-            var key = block.unwrapKey().orElseThrow().location();
-            tag(loaderTag("dyed")).addOptional(key);
-            tag(loaderTag("dyed/" + dye)).addOptional(key);
+            var id = block.unwrapKey().orElseThrow().location();
+            var tag = loaderTag("dyed/" + dye);
+            tag(tag).addOptional(id);
         });
 
         for (var tag : additionalTags) {
