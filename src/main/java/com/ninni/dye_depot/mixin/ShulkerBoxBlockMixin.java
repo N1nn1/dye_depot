14134335b77de5747b428e402626273aaa730541
Niklas Widmann <nik@somethingcatchy.net>
2026-05-10 21:51:37 +0200

    fix shulkers not being able to be dyed

--- src/main/java/com/ninni/dye_depot/mixin/ShulkerBoxBlockMixin.java
+++ src/main/java/com/ninni/dye_depot/mixin/ShulkerBoxBlockMixin.java
@@ -1,8 +1,9 @@
 package com.ninni.dye_depot.mixin;
 
 import com.ninni.dye_depot.registry.DDBlocks;
+import com.ninni.dye_depot.registry.DDDyes;
 import net.minecraft.world.item.DyeColor;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.ShulkerBoxBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,14 +14,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ShulkerBoxBlockMixin {
 
     @Inject(
-            method = "getColoredItemStack",
+            method = "getBlockByColor",
             at = @At("HEAD"),
             cancellable = true
     )
-    private static void injectModShulkers(DyeColor dye, CallbackInfoReturnable<ItemStack> cir) {
-        if (dye == null) return;
-        var box = DDBlocks.SHULKER_BOXES.getOrNull(dye);
-        if (box != null) cir.setReturnValue(new ItemStack(box));
+    private static void injectModShulkers(DyeColor dye, CallbackInfoReturnable<Block> cir) {
+        if (dye == null || !DDDyes.isModDye(dye)) return;
+        var box = DDBlocks.SHULKER_BOXES.getOrThrow(dye);
+        cir.setReturnValue(box);
     }
 
 }
