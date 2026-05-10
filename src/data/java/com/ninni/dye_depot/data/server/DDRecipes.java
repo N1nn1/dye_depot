1be697fa7404128da5fde886b9a809b67886043b
Niklas Widmann <nik@somethingcatchy.net>
2026-05-10 22:48:16 +0200

    overwrite vanilla wool recipes to use tag instead of hardcoded ingredient list (#91)

--- src/data/java/com/ninni/dye_depot/data/server/DDRecipes.java
+++ src/data/java/com/ninni/dye_depot/data/server/DDRecipes.java
@@ -111,7 +111,7 @@ public class DDRecipes extends FabricRecipeProvider {
             );
         });
 
-        dyeing(output, RecipeCategory.MISC, DDBlocks.WOOL, ItemTags.WOOL);
+        dyeing(output, RecipeCategory.MISC, DDBlocks.WOOL.mergeVanilla(blockLookup), ItemTags.WOOL);
 
         dyeConversion(output, DDDyes.AMBER.get(), Items.GLOW_BERRIES, 1);
         dyeConversion(output, DDDyes.AMBER.get(), Items.HONEYCOMB, 2);
