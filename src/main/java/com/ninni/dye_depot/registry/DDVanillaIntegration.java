91ebbec2191f02e6ce0f523e79d9d35168f6077f
Niklas Widmann <nik@somethingcatchy.net>
2026-05-10 22:43:13 +0200

    add cauldron interactions for banners & shulkers to allow them to be washed (#92)

--- src/main/java/com/ninni/dye_depot/registry/DDVanillaIntegration.java
+++ src/main/java/com/ninni/dye_depot/registry/DDVanillaIntegration.java
@@ -1,5 +1,6 @@
 package com.ninni.dye_depot.registry;
 
+import net.minecraft.core.cauldron.CauldronInteractions;
 import net.minecraft.core.dispenser.ShulkerBoxDispenseBehavior;
 import net.minecraft.world.level.block.DispenserBlock;
 
@@ -7,12 +8,21 @@ public class DDVanillaIntegration {
 
     public static void commonInit() {
         registerDispenserBehaviours();
+        registerCauldronInteractions();
     }
 
     private static void registerDispenserBehaviours() {
-        ShulkerBoxDispenseBehavior shulkerBoxBehaviour = new ShulkerBoxDispenseBehavior();
+        var behavior = new ShulkerBoxDispenseBehavior();
         DDBlocks.SHULKER_BOXES.values().forEach((block) ->
-            DispenserBlock.registerBehavior(block, shulkerBoxBehaviour)
+            DispenserBlock.registerBehavior(block, behavior)
         );
     }
+
+    private static void registerCauldronInteractions() {
+        var interactions = CauldronInteractions.WATER;
+
+        DDItems.SHULKER_BOXES.values().forEach(it -> interactions.put(it, CauldronInteractions::shulkerBoxInteraction));
+        DDItems.BANNERS.values().forEach(it -> interactions.put(it, CauldronInteractions::bannerInteraction));
+    }
+
 }
